65a3b406ba56e1daefe6d6257e8c167212b242d7
Pedro Mota <20251002803166@pucgo.edu.br>
2026-07-06 22:09:10 -0300

    docs: add system modeling and architecture documentation

---
+++ src/main/java/com/clinichub/ClinichubApplication.java
@@ -0,0 +1,13 @@
+package com.clinichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinichubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinichubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinichub/ClinichubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinichubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
